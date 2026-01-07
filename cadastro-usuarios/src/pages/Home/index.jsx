83729fcc0e65e3eeae432a4b25dc99e91993103d
brenofreitast <brenofreitast@gmail.com>
2026-01-07 16:27:39 -0300

    React

---
+++ cadastro-usuarios/src/pages/Home/index.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState, useRef } from 'react'
+import './style.css'
+import Trash from '../../assets/excluir.png'
+import { use } from 'react'
+import api from '../../services/api.js'
+
+function Home() {
+
+  const [users, setUsers] = useState([])
+  const inputName = useRef()
+  const inputAge = useRef()
+
+  async function getUsers(){
+    const usersFromApi = await api.get('/users')
+    setUsers(usersFromApi.data)
+    
+    console.log(users)
+  }
+
+  async function createUsers() {
+    await api.post('/users', {
+      name: inputName.current.value,
+      age: inputAge.current.value
+    })
+    getUsers()
+  }
+
+  useEffect (() => {
+    getUsers()
+  }, [])
+
+  return (
+
+    <div className='container'>
+      <form action="">
+        <h1>Cadastro de Usuários</h1>
+        <input placeholder='Nome' name='nome' type='text' ref={inputName} />
+        <input placeholder='Idade' name='idade' type='number' ref={inputAge} />
+        <button type='button' onClick={createUsers}>Cadastrar</button>
+      </form>
+
+      {users.map(user => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span> </p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
+
+        </div>
+      ))}
+
+
+    </div>
+
+
+  )
+}
+
+export default Home
